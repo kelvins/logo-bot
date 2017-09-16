468f697bf40e76cf383e325c12aa0cf376d0dec8
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-09-16 19:28:30 -0300

    Improving the tests

--- tests/logo_test.py
+++ tests/logo_test.py
@@ -1,3 +1,5 @@
+
+import os
 import sys
 import unittest
 
@@ -5,10 +7,76 @@ sys.path.append('./src')
 from logo import Logo
 
 class LogoTest(unittest.TestCase):
-    def test1(self):
-        logo = Logo("", "", "")
-        logo_bot = LogoBot(logo)
-        logo_bot.run()
+
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    def test_run(self):
+        output_path = self.path + "/../output/"
+        logo = Logo(self.path + "/../input/", self.path + "/../logo.png", output_path, 20, "bottom_right", "png")
+
+        for dirname, dirnames, filenames in os.walk(output_path):
+            for filename in filenames:
+                try:
+                    os.remove(dirname+filename)
+                except Exception:
+                    pass
+
+        self.assertEqual(len(os.listdir(output_path)), 0)
+
+        logo.add()
+        self.assertEqual(len(os.listdir(output_path)), 3)
+
+    def test_calc_size(self):
+        logo = Logo(self.path + "/../input/", self.path + "/../logo.png", self.path + "/../output/", 50, "bottom_right", "png")
+
+        width, height = logo.calc_size((800, 600), (0, 150))
+        self.assertEqual(width, 0)
+        self.assertEqual(height, 0)
+
+        width, height = logo.calc_size((100, 100), (100, 100))
+        self.assertEqual(width, 50)
+        self.assertEqual(height, 50)
+
+        width, height = logo.calc_size((100, 100), (50, 10))
+        self.assertEqual(width, 50)
+        self.assertEqual(height, 10)
+
+        width, height = logo.calc_size((800, 600), (100, 150))
+        self.assertEqual(width, 200)
+        self.assertEqual(height, 300)
+
+    def test_calc_position(self):
+        logo = Logo(self.path + "/../input/", self.path + "/../logo.png", self.path + "/../output/", 20, "bottom_right", "png")
+
+        logo.position = "center"
+        x, y = logo.calc_position((800, 600), (0, 150))
+        self.assertEqual(x, 0)
+        self.assertEqual(y, 0)
+
+        logo.position = "center"
+        x, y = logo.calc_position((100, 100), (50, 50))
+        self.assertEqual(x, 25)
+        self.assertEqual(y, 25)
+
+        logo.position = "top_left"
+        x, y = logo.calc_position((100, 100), (50, 50))
+        self.assertEqual(x, 0)
+        self.assertEqual(y, 0)
+
+        logo.position = "bottom_right"
+        x, y = logo.calc_position((100, 100), (50, 50))
+        self.assertEqual(x, 50)
+        self.assertEqual(y, 50)
+
+        logo.position = "bottom_left"
+        x, y = logo.calc_position((100, 100), (50, 50))
+        self.assertEqual(x, 0)
+        self.assertEqual(y, 50)
+
+        logo.position = "center_right"
+        x, y = logo.calc_position((100, 100), (50, 50))
+        self.assertEqual(x, 50)
+        self.assertEqual(y, 25)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/logobot_test.py
+++ tests/logobot_test.py
@@ -1,38 +1,34 @@
 import sys
 import unittest
 import os
+import time
 
 sys.path.append('./src')
 from logo import Logo
 from logobot import LogoBot
 
-class LogoBotTest(unittest.TestCase):
-    
-    path = os.path.dirname(os.path.abspath(__file__))
 
-    def test_number_of_files(self):
-        logo_bot = LogoBot(None)
-        self.assertEqual(logo_bot.number_of_files(""), 0)
-        self.assertEqual(logo_bot.number_of_files(self.path + "\input"), 3)
+class LogoBotTest(unittest.TestCase):
 
     def test_run(self):
-        logo = Logo(self.path + "\input", self.path + "\logo.png", self.path + "\output")
+        path = os.path.dirname(os.path.abspath(__file__))
+        output_path = path + "/../output/"
+        logo = Logo(path + "/../input/", path + "/../logo.png", output_path, 20, "bottom_right", "png")
         logo_bot = LogoBot(logo)
-        
-        output_path = self.path + "\output"
 
-        if output_path > 0:
-            for dirname, dirnames, filenames in os.walk(output_path):
-                for filename in filenames:
-                    try:
-                        os.remove(filename)
-                    except Exception:
-                        pass
+        for dirname, dirnames, filenames in os.walk(output_path):
+            for filename in filenames:
+                try:
+                    os.remove(dirname+filename)
+                except Exception:
+                    pass
 
-        self.assertEqual(logo_bot.number_of_files(output_path), 0)
+        self.assertEqual(len(os.listdir(output_path)), 0)
 
-        logo_bot.run()
-        self.assertEqual(logo_bot.number_of_files(output_path), 3)
+        logo_bot.start()
+        time.sleep(5)
+        self.assertEqual(len(os.listdir(output_path)), 3)
+        logo_bot.stop()
 
 if __name__ == '__main__':
     unittest.main()
